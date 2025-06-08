c9d981cb918667b00c8ae8e82f68aa4e913abb4c
Alexander Tasinchano <132286484+Alexande6055@users.noreply.github.com>
2025-06-08 16:56:46 -0500

    se añade la metadata para que la pagina web sea indexada pór google

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import type React from "react"
 import { ChatbotProvider } from "@/contexts/chatbot-provider"
 import { ChatbotWidget } from "@/components/chatbot/chatbot-widget"
 import "./globals.css"
+export const metadata = {
+  title: "Plataforma de Gestión de Cursos y Eventos Educativos",
+  description: "App web completa para gestionar cursos, inscripciones y certificados, con roles, auditoría, y backend desacoplado. Desarrollada como demo técnica para portafolio profesional.",
+  other: {
+    "google-site-verification": "vHIzuFR42_8-BkNGQSs0buGLpfEmZKxiuaPRV2YPXss",
+  },
+};
 
 export default function RootLayout({
   children,
